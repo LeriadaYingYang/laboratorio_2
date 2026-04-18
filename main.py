099d8f74dcab42283dc7598718c92072016d12fa
Daniel Jara Alva <n00243181@upn.pe>
2026-04-18 17:52:29 -0500

    Iniciando el menu

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("Hello World")
+def opcion_1():
+    print("Elegiste la opción 1")
+
+def opcion_2():
+    print("Elegiste la opción 2")
+
+def opcion_3():
+    print("Elegiste la opción 3")
+
+def opcion_4():
+    print("Elegiste la opción 4")
+
+
+def mostrar_menu():
+    print("\n===== MENÚ PRINCIPAL =====")
+    print("1. Opción 1")
+    print("2. Opción 2")
+    print("3. Opción 3")
+    print("4. Opción 4")
+    print("0. Salir")
+
+
+def main():
+    while True:
+        mostrar_menu()
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            opcion_1()
+        elif opcion == "2":
+            opcion_2()
+        elif opcion == "3":
+            opcion_3()
+        elif opcion == "4":
+            opcion_4()
+        elif opcion == "0":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción inválida, intenta de nuevo.")
+
+
+# Ejecutar programa
+main()
