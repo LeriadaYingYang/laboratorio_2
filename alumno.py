4717381cc2843d0a5096c447b46f3f2c3b216e4b
Edith <N00448342@UPN.PE>
2026-04-19 22:46:58 -0500

    Correccion

--- alumno.py
+++ alumno.py
@@ -1,34 +1,13 @@
-class Alumno:
-    def __init__(self, nombre, nota1,nota2,nota3):
+class Alumno: 
+    def __init__(self, codigo, nombre, apellido):
+        self.codigo = codigo
         self.nombre = nombre
-        self.nota1= nota1
-        self.nota2=nota2
-        self.nota3=nota3
-
-    def calcular_promedio(self):
-        return(self.nota1 + self.nota2 + self.nota3)/3
+        self.apellido = apellido
     
-    def estado(self):
-        if self.calcular_promedio()>= 11:
-            return "Aprobado"
-        else:
-            return "Desaprobado"
-        
-    def mostrar_datos(self):
-        print("Nombre:", self.nombre)
-        print("Notas:",
-             int(self.nota1),
-             int(self.nota2),
-             int(self.nota3))
-
-        print("Promedio:", round(self.calcular_promedio(),2))
-        print("Estado:", self.estado())
+    def ver_alumno(self):
+        return f"codigo: {self.codigo} - Nombre: {self.nombre} {self.apellido}"
     
-if __name__== "__main__":
-        nombre=input("Ingrese el nombre del alumno: ")
-        n1 = float(input("Ingrese nota 1: "))
-        n2 = float(input("Ingrese nota 2: "))
-        n3 = float(input("Ingrese nota 3: "))
+    def obtener_codigo(self):
+        return self.codigo
+    
         
-        alumno1= Alumno(nombre, n1,n2,n3)
-        alumno1.mostrar_datos()
