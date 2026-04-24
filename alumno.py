43d600ecf7486b8a31d6365c42ce1927953d8021
Edith <N00448342@UPN.PE>
2026-04-23 21:28:08 -0500

    eliminar estudiante.py

---
+++ alumno.py
@@ -0,0 +1,34 @@
+#==============================================
+#Modulo: alumno.py
+#Descripcion: Manejo de datos del alumno
+#==============================================
+
+
+def crear_estudiante(codigo, nombre, apellido, carrera):
+    estudiante = { 
+        "Codigo":codigo, 
+        "Nombre":nombre, 
+        "Apellido":apellido, 
+        "Carrera":carrera,
+        }
+    return estudiante  
+
+def ingresar_estudiante():
+    print("======INGRESO DE DATOS DEL ESTUDIANTE:======")
+
+    codigo = input("Codigo: ")
+    nombre = input("Nombre: ")
+    apellido = input("Apellido: ")
+    carrera = input("Carrera: ")
+
+    estudiante = crear_estudiante(codigo, nombre, apellido, carrera) 
+    return estudiante
+
+def mostrar_estudiantes(lista_estudiantes):
+
+    print("======LISTA DE ESTUDIANTES:======")
+    for est in lista_estudiantes:
+        print(f"Codigo :  {est['Codigo']}")
+        print(f"Nombre :  {est['Nombre']}")
+        print(f"Apellido :  {est['Apellido']}")
+        print(f"Carrera :  {est['Carrera']}")
